284206d37c161b4945efac68d3ce6ef92da00d9b
8203255945 <8203255945@ci121022m.ecc.u-tokyo.ac.jp>
2014-10-24 11:18:25 +0900

    fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz
+  for i in 0..99 do
+    if i % 3 != 0 && i % 5 != 0 then
+        printf("%d", i)
+    end
+    if i % 3 == 0 then
+        printf("fizz")
+    end
+    if i % 5 == 0 then
+        printf("buzz")
+    end
+    printf("\n")
+  end
+end
