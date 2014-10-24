a1eb80508d03cc2df52abf21953c4e7d91342d10
8203255945 <8203255945@ci121022m.ecc.u-tokyo.ac.jp>
2014-10-24 10:45:19 +0900

    primenumber

---
+++ primenum.rb
@@ -0,0 +1,27 @@
+def primejudge(n)
+  for i in 2..(Math.sqrt(n)) do
+    if n % i == 0 then 
+      return 0
+    end
+  end
+  return 1
+end
+
+def primeun(n)
+  for i in 3..n do
+    if primejudge(i) == 1 then
+      printf("%d\n", i)
+    end
+  end
+end
+
+def eprimeun(n)
+  list = [*2..n]
+  plist = []
+  until list.first > Math.sqrt(n)
+    s = list.shift
+    plist << s
+    list.delete_if {|m| (m % s).zero? }
+  end
+  return plist + list
+end
