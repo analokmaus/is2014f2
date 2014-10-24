eb844c51dbaf94d0028f3f43b8720faf7a76f008
8203255945 <8203255945@ci111064m.ecc.u-tokyo.ac.jp>
2014-10-24 18:06:48 +0900

    sort

--- array.rb
+++ array.rb
@@ -1,14 +1,40 @@
-def arraymax(a)
-  a.sort!
-  return a.last
+def arraybsort(a)
+  for i in 0..(a.length - 1) do
+    for j in 0..(a.length - (i + 2)) do
+      if a[j] > a[j+1] then
+        temp = a[j+1]
+        a[j+1] = a[j]
+        a[j] = temp
+      end
+    end
+  end
+  return a
+end
+    
+def arrayqsort(a)
+  if a.length <= 1 then
+    return a
+  end
+  pivot = a[0]
+  right = []
+  left = []
+  for i in 1..(a.length - 1) do
+    if a[i] <= pivot then
+      left << a[i]
+    else
+      right << a[i]
+    end
+  end
+  left = arrayqsort(left)
+  right = arrayqsort(right)
+  return left + [pivot] + right
 end
 
 def arraymaxorder(a)
-  b = a
-  max = arraymax(b)
+  max = arraymax(a)
   for i in 0..(a.length - 1) do
     if a[i] == max then
-      return i
+      return i + 1
     end
   end
 end
