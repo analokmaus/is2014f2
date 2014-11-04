18be6a371c032e81a98983040a5649bd9142d116
8203255945 <8203255945@ci111090m.ecc.u-tokyo.ac.jp>
2014-11-04 20:47:31 +0900

    added anagrams generator ( in progress )

--- recursive.rb
+++ recursive.rb
@@ -14,3 +14,37 @@ def dectobin(n)
   $init = 0
   return $ans
 end
+
+$vowel=["a","i","u","e","o"]
+
+def anagrams(name)
+  genanagrams(name.split(""), [])
+end
+
+def genanagrams(name, temp)
+  if name.length == temp.length then
+    p(temp)
+    return 
+  end
+  name.each_index do |i|
+    if name[i] == nil then
+      next
+    end
+    
+    if $vowel.include?(name[i]) == false && $vowel.include?(temp.last) == false && temp.last != nil then
+      next
+    end
+
+    #if $vowel.include?(name[i]) == false && temp.length == name.length - 1 then
+    #  next
+    #end
+
+    x = name[i]
+    name[i] = nil
+    temp.push(x)
+    genanagrams(name, temp)
+    name[i] = x
+    temp.pop
+  end
+end
+  
