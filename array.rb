61dbaa7b5ef8cb267c14244cba40f377b8657de4
8203255945 <8203255945@ci121116m.ecc.u-tokyo.ac.jp>
2014-11-14 12:06:40 +0900

    array update

--- array.rb
+++ array.rb
@@ -1,11 +1,14 @@
+### arraygenerator ###
+def randarray(n, max)
+  return Array.new(n) do rand(max) end
+end
+
 ### array bubble sort ###
 def arraybsort(a)
-  for i in 0..(a.length - 1) do
-    for j in 0..(a.length - (i + 2)) do
+  for i in 0...a.length do
+    for j in 0...a.length - (i + 1) do
       if a[j] > a[j+1] then
-        temp = a[j+1]
-        a[j+1] = a[j]
-        a[j] = temp
+        temp = a[j+1]; a[j+1] = a[j]; a[j] = temp #swap
       end
     end
   end
@@ -32,6 +35,26 @@ def arrayqsort(a)
   return left + [pivot] + right
 end
 
+### array merge sort ###
+def arraymsort(a, i, j)
+    if j <= i
+      return
+    else
+      k = (i + j) / 2
+      arraymsort(a, i, k); arraymsort(a, k + 1, j)
+      left = a[i..k]; right = a[k+1..j]
+      while i <= k || k + 1 <= j do
+        if i > k || k + 1 <= j && a[i] > a[k + 1] then
+          b.push(a[j]); j += 1
+        else
+          b.push(a[i]); i += 1
+        end
+      end
+    end
+    return a
+end
+      
+    
 ### max in array ###
 def arraymax(a)
   max = a[0]
@@ -43,3 +66,10 @@ def arraymax(a)
   return max
 end
   
+### benchmark ###
+def bench(count, &block)
+  t1 = Process.times.utime
+  count.times do yield end
+  t2 = Process.times.utime
+  return t2 - t1
+end
