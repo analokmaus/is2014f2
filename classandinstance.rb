daccac8128ae2a08be2512cf2d20c38856cf2ea9
8203255945 <8203255945@ci111050m.ecc.u-tokyo.ac.jp>
2014-12-17 14:50:53 +0900

    bruteforce attack

--- classandinstance.rb
+++ classandinstance.rb
@@ -79,3 +79,74 @@ class Human
     @nationality
   end
 end
+
+class Buffer
+  Cell = Struct.new(:data, :next)
+  def initialize
+    @tail = @cur = Cell.new("EOF", nil)
+    @head = @prev = Cell.new("", @cur)
+  end
+  def atend
+    return @cur == @tail
+  end
+  def top
+    @prev = @head; @cur = @head.next
+  end
+  def forward
+    if atend then return end
+    @prev = @cur; @cur = @cur.next
+  end
+  def insert(s)
+    @prev.next = Cell.new(s, @cur); @prev = @prev.next
+  end
+  def print
+    puts(" " + @cur.data)
+  end
+  def delete
+    @cur = Cell.new("EOF", @cur.next)
+    @prev.next = @cur
+  end
+  def exchange
+    tmp = @prev.data
+    @prev.data = @cur.data
+    @cur.data = tmp
+  end
+end
+
+def hackme(n)
+  # generate password
+  for i in 0...n do
+    $pwd = rand(100000)
+    
+  end
+end
+  
+#$cand = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYX0123456789"
+$cand = "0123456789"
+$cand = $cand.split('')
+def brute(n)
+  if n == 0
+    puts $temp
+    if $temp == $pass
+      puts "Password: #{$temp}"
+      puts "Time: #{Time.now-$old}msec"
+      exit
+    end
+    return
+  end
+  $cand.each do |c|
+    $temp += c
+    brute(n-1)
+    $temp = $temp.slice(0, $temp.size-1)
+  end
+end
+
+def bruteforce(a)
+  $pass = a
+  $temp = ""
+  $old = Time.now
+  for i in 1..$pass.size
+    puts "--- Try#{i} ---"
+    brute(i)
+  end
+end
